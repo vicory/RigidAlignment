70aa40743a1ac9d1f090f29c23dc707495aae6b1
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-08-15 04:48:27 -0400

    cmake: Fix, modernize and simplify build-system
    This commits updates the build-system so that the CLI can be successfully
    bundled within a Slicer extension, a Slicer custom application or as a
    standalone project.
    
    * update minimum required version from CMake 2.8 to 3.5
    
    * rename CLI from RigidWrapper to RigidAlignment
    
    * rename header RigidAlignment.h to RigidAlignmentImpl.h where Impl
      stands for "Implementation"
    
    * remove the extra static library "RigidAlignment" and directly include the
      source in the CLI.
    
    * strip trailing white spaces
    
    * convert tabs to spaces

---
+++ RigidAlignment.cxx
@@ -0,0 +1,36 @@
+#include <string>
+#include <dirent.h>
+
+#include "RigidAlignmentCLP.h"
+#include "RigidAlignmentImpl.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  PARSE_ARGS;
+
+  if (argc < 3)
+  {
+    cout << "Usage: " << argv[0] << " --help" << endl;
+    return -1;
+  }
+
+  DIR *dir;
+  dirent *pdir;
+  vector<char *> landmarkList;
+
+  dir = opendir(landmark.c_str());
+
+  while (pdir = readdir(dir))
+  {
+    if ('.' == pdir->d_name[0]) continue;
+    landmarkList.push_back(pdir->d_name);
+  }
+
+  RigidAlignment *RAlign = new RigidAlignment(landmark.c_str(), landmarkList, sphere.c_str(), output.c_str(), lmtype);
+
+  if (!outputLM.empty()) RAlign->saveLM(outputLM.c_str());
+
+  delete RAlign;
+}
